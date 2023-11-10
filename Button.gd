9fd77f5a4ddfc7c4da197e562fcb4917e3024564
Fizzy78 <isaacthegiant@gmail.com>
2023-11-10 12:18:41 -0600

    Two Buttons with text

--- Button.gd
+++ Button.gd
@@ -1,11 +1,9 @@
 extends Button
 
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://game.tscn")
+	
 
 
 func _on_quit_pressed():
 	get_tree().quit()
-
-
-func _on_start_pressed():
-	get_tree().change_scene_to_file("res://game.tscn")
-	
