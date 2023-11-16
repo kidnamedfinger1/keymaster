5947de78dec61ea7b6f6ef7a96d728f2b959bcf4
kidnamedfinger1 <111032514+kidnamedfinger1@users.noreply.github.com>
2023-11-16 11:52:29 -0600

    Add files via upload

---
+++ keymaster-main/lettergen.gd
@@ -0,0 +1,17 @@
+extends Label
+
+var letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+var randomIndex : String
+
+func makeletter():
+	randomIndex = letters[randi() % letters.size()]
+	set_text(randomIndex)
+
+func _ready():
+	makeletter()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and OS.get_keycode_string(event.key_label) == randomIndex:
+			makeletter()
