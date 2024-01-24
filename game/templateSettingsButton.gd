0bc3d634bba6da394e6061c69832367fa307882d
Emi-4 <brendanakee@gmail.com>
2024-01-24 12:33:42 -0600

    fully functional - quitToMenu & quitToDesktop
    working on audio sliders next

--- game/templateSettingsButton.gd
+++ game/templateSettingsButton.gd
@@ -23,5 +23,10 @@ func _on_keymonkey_menu_pressed():
 func _on_quitToMenu_pressed():
 	get_tree().change_scene_to_file("res://menu/mainMenu.tscn")
 
-func _on_quitToDesktop_pressed():
-	get_tree().quit
+
+
+
+
+func _on_pressed():
+	get_tree().quit()
+
