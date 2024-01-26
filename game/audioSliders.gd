0c277ea350b98a8477fe59f9f0726ab4b17092aa
Emi-4 <brendanakee@gmail.com>
2024-01-26 12:00:36 -0600

    sprint 8 - sliders connected to settings menu

--- game/audioSliders.gd
+++ game/audioSliders.gd
@@ -1,15 +1,23 @@
 extends HSlider
 
-
+var visibleMenu = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 
 func _on_settings_menu_pressed():
-	pass # Replace with function body.
+	if visibleMenu == false:
+		visibleMenu = true
+	elif visibleMenu == true:
+		visibleMenu = false
+
+func _process(delta):
+	if visibleMenu == false:
+		self.visible = false
+		self.z_index = -1
+	elif visibleMenu == true:
+		self.z_index = 0
+		self.visible = true
