d33f7814190de31d78bd573ea227bb30b49e4646
Fizzy78 <isaac.hooper@student.allenisd.org>
2024-02-09 12:38:02 -0600

    Slider functionality, label visibility

--- game/audioSliders.gd
+++ game/audioSliders.gd
@@ -21,3 +21,11 @@ func _process(delta):
 	elif visibleMenu == true:
 		self.z_index = 0
 		self.visible = true
+	if get_node("../sfxSlider").value == 0:
+		get_node("../soundEffectPlayer").volume_db = -1000
+	else:
+		get_node("../soundEffectPlayer").volume_db = -18.432 + (0.2 * (get_node("../sfxSlider").value - 50))
+	if get_node("../musicSlider").value == 0:
+		get_node("../musicPlayer").volume_db = -1000
+	else:
+		get_node("../musicPlayer").volume_db = -28.543 + (0.2 * (get_node("../musicSlider").value - 50))
