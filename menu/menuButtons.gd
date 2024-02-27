09edca293ee6ea1bc1f8a0a12ac04f3be2c0c410
Franklin Hernandez <omarhernandezH0810@gmail.com>
2024-02-27 13:07:24 -0600

    New Menu Screen (optional)
    This might be used in v0.3. The game requires the player to press the key "k", in order to start the game. The "quit" button on the main menu now has a texture.

--- menu/menuButtons.gd
+++ menu/menuButtons.gd
@@ -1,7 +1,6 @@
 extends Button
 
-func _on_start_pressed():
-	get_tree().change_scene_to_file("res://game/game.tscn")
+
 	
 
 
@@ -9,3 +8,8 @@ func _on_quit_pressed():
 	get_tree().quit()
 
 
+
+
+func _input(event):
+	if Input.is_key_pressed(KEY_K):
+		get_tree().change_scene_to_file("res://game/game.tscn")
