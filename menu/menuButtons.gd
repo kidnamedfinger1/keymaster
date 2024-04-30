e2905422bf0f10a989cece678361f37b2425e835
Fizzy78 <isaac.hooper@student.allenisd.org>
2024-04-30 12:54:34 -0500

    Menu fullscreen on start

--- menu/menuButtons.gd
+++ menu/menuButtons.gd
@@ -1,6 +1,7 @@
 extends Button
 
-
+func _ready():
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 
 func _on_quit_pressed():
 	get_tree().quit()
@@ -8,6 +9,6 @@ func _on_quit_pressed():
 
 
 
-func _input(event):
+func _input(_event):
 	if Input.is_key_pressed(KEY_K):
 		get_tree().change_scene_to_file("res://game/game.tscn")
